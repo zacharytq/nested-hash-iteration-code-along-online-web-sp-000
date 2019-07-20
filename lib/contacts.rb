bb1b2f76d757f0cc36dbab88a78fd56435c6c873
chic-page-6502 <zacharytq@gmail.com>
2019-07-20 01:25:55 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,9 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |contact, contacts_hash|
+    if contact == "Freddy Mercury"
+      contacts_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
 end
